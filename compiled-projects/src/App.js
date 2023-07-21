3ff38bc3a9e5215dd05a57e2b3d66921ebb6140a
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-07-21 22:57:44 +0800

    Continue the usePopcorn project

--- compiled-projects/src/App.js
+++ compiled-projects/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+// how react works
+import "./how-react-works/index.css";
+
 import "./usepopcorn/stylePopcorn.css";
+import App1 from "./how-react-works/App";
 
 import PopApp from "./usepopcorn/App";
 
 function App() {
   return (
     <div className="">
-      <PopApp />
+      <App1 />
     </div>
   );
 }
